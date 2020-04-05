992e568887b8df25e1a701a19f7fffa7465bdbca
ddamjengi <silluat11@gmail.com>
2020-04-05 11:26:55 +0900

    .

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,25 +1,43 @@
 const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 const csso = require("gulp-csso");
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
 const del = require("del");
+const ghPages = require("gulp-gh-pages");
 
 const paths = {
   img: {
     src: "images/**/*",
-    dest: "bundle/images"
+    dest: "bundle/images",
   },
   css: {
     src: "assets/*.css",
-    dest: "bundle/css"
+    dest: "bundle",
   },
   js: {
     src: "assets/js/*.js",
-    dest: "bundle/js"
-  }
+    dest: "bundle",
+  },
 };
 
 function clean() {
   return del(["bundle"]);
+}
+
+function publishClean() {
+  return del([".publish"]);
+}
+
+function js() {
+  return gulp.src(paths.js.src).pipe(gulp.dest(paths.js.dest));
+}
+
+function image() {
+  return gulp
+    .src(paths.img.src)
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.img.dest));
 }
 
 function styles() {
@@ -27,11 +45,22 @@ function styles() {
     .src(paths.css.src)
     .pipe(
       autoprefixer({
-        overrideBrowserlist: ["last 2 versions"]
+        overrideBrowserlist: ["last 2 versions"],
       })
     )
+    .pipe(concat("styles.css"))
     .pipe(csso())
-    .pipe(gulp.dest(paths.scss.dest));
+    .pipe(gulp.dest(paths.css.dest));
 }
 
-export const dev = gulp.series([clean, styles]);
+function deploy() {
+  return gulp.src("./**/*").pipe(ghPages());
+}
+
+const dev = gulp.series([clean, styles, image, js]);
+
+const _deploy = gulp.series([publishClean, deploy]);
+
+module.exports.dev = dev;
+
+module.exports.deploy = _deploy;
